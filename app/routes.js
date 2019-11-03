00d836d385a73de3f38bf44e67417aa104027091
Nguyễn Trường Giang <44118083+bingii98@users.noreply.github.com>
2019-11-04 00:18:32 +0700

    ABC

--- app/routes.js
+++ app/routes.js
@@ -28,7 +28,6 @@ app.use(session({
 
 AWS.config.update({
     "region": "us-east-1",
-    //"endpoint": "http://localhost:8000",
     "endpoint": "http://dynamodb.us-east-1.amazonaws.com"
 });
 
--- controller/Customer-controller.js
+++ controller/Customer-controller.js
@@ -3,7 +3,6 @@ const AWS = require('aws-sdk'),
 
 AWS.config.update({
     "region": "us-east-1",
-    //"endpoint": "http://localhost:8000",
     "endpoint": "http://dynamodb.us-east-1.amazonaws.com"
 });
 
